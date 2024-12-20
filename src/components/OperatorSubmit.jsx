5584c9731e87f7e52f56d544cbf8d14a5350ed88
sHRLo <62970136+sHRLo@users.noreply.github.com>
2024-12-21 01:55:48 +0330

    Update OperatorSubmit.jsx

--- src/components/OperatorSubmit.jsx
+++ src/components/OperatorSubmit.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
 import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function Submit() {
+const Submit = () => {
   const [values, setValues] = useState({
     formcode: "",
     section: "",
+    machinename: "",
     shift: "",
     operatorname: "",
     formdate: "",
@@ -14,232 +16,226 @@ function Submit() {
     startdate: "",
     problemdescription: "",
   });
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    // const formData = new FormData();
+    // formData.append("formcode", values.formcode);
+    // formData.append("section", values.section);
+    // formData.append("machinename", values.machinename);
+    // formData.append("shift", values.shift);
+    // formData.append("operatorname", values.operatorname);
+    // formData.append("formdate", values.formdate);
+    // formData.append("problemtype", values.problemtype);
+    // formData.append("stopstatus", values.stopstatus);
+    // formData.append("stopdate", values.stopdate);
+    // formData.append("startdate", values.startdate);
+    // formData.append("problemdescription", values.problemdescription);
+
     try {
-      const response = await axios.post("http://localhost:3000/submit", values);
-      console.log(response);
+      const response = await axios.post(
+        "http://localhost:3000/auth/submit",
+        values
+      );
+      navigate("/dashboard");
     } catch (err) {
-      console.log(err.message);
+      alert(Result.data.Error);
     }
   };
+
   return (
-    <div>
-      <h1 className="text-center text-4xl">Operator Form</h1>
-      <form
-        onSubmit={handleSubmit}
-        className={
-          "bg-white rounded-[15px] shadow-2xl p-3.5 transition-transform duration-0.2 w-[650px] text-center m-auto mt-5"
-        }
-      >
-        <label
-          htmlFor="formcode"
-          className="text-sm block w-full mt-2 mb-1 text-left font-bold"
-        >
-          Form Code*
-        </label>
-        <input
-          className="block w-full p-2 box-border rounded-sm text-xs"
-          type="text"
-          placeholder="Enter Code Number"
-          name="formcode"
-          onChange={(e) => setValues({ ...values, formcode: e.target.value })}
-        />
-
-        <label
-          htmlFor="section"
-          className="text-sm block w-full mt-2 mb-1 text-left font-bold"
-        >
-          section*
-        </label>
-        <select
-          name="section"
-          className="block w-full mb-4 p-2 box-border rounded"
-          onChange={(e) => setValues({ ...values, section: e.target.value })}
-        >
-          <option value="chipper">Chipper</option>
-          <option value="conveyorline">Conveyor Line</option>
-          <option value="dryerairgraider">Dryer Air Grader</option>
-          <option value="refiner">Refiner</option>
-          <option value="beforepress">Before Press</option>
-          <option value="press">Press</option>
-          <option value="afterpress">After Press</option>
-          <option value="sandingrbs">Sanding</option>
-          <option value="coolingsystem">Cooling System</option>
-          <option value="steamboiler">Steam Boiler</option>
-          <option value="general">General</option>
-        </select>
-
-        <label
-          htmlFor="machinename"
-          className="text-sm block w-full mt-2 mb-1 text-left font-bold"
-        >
-          Machine Name
-        </label>
-        <input
-          type="text"
-          className="block w-full p-2 box-border rounded-sm text-xs"
-          name="machinename"
-          placeholder="Enter Machine Name"
-          onChange={(e) =>
-            setValues({ ...values, machinename: e.target.value })
-          }
-        />
-
-        <label
-          htmlFor="equipmentname"
-          className="text-sm block w-full mt-2 mb-1 text-left font-bold"
-        >
-          Equipment Name
-        </label>
-        <input
-          type="text"
-          className="block w-full p-2 box-border rounded-sm text-xs"
-          name="equipmentname"
-          placeholder="Enter Equipment Name"
-          onChange={(e) =>
-            setValues({ ...values, equipmentname: e.target.value })
-          }
-        />
-
-        <label
-          htmlFor="shift"
-          className="text-sm block w-full mt-2 mb-1 text-left font-bold"
-        >
-          Shift
-        </label>
-        <select
-          className="block w-full mb-4 p-2 box-border rounded"
-          name="shift"
-          onChange={(e) => setValues({ ...values, shift: e.target.value })}
-        >
-          <option value="a">A</option>
-          <option value="b">B</option>
-          <option value="c">C</option>
-        </select>
-
-        <label
-          htmlFor="operatorname"
-          className="text-sm block w-full mt-2 mb-1 text-left font-bold"
-        >
-          Operator Name
-        </label>
-        <input
-          className="block w-full p-2 box-border rounded-sm text-xs"
-          type="text"
-          name="operatorname"
-          placeholder="Enter Operator Name"
-          onChange={(e) =>
-            setValues({ ...values, operatorname: e.target.value })
-          }
-        />
-
-        <label
-          htmlFor="formdate"
-          className="text-sm block w-full mt-2 mb-1 text-left font-bold"
-        >
-          Form Date
-        </label>
-        <input
-          type="datetime-local"
-          className="block w-full p-2 box-border rounded-sm text-xs"
-          name="formdate"
-          onChange={(e) => setValues({ ...values, formdate: e.target.value })}
-        />
-
-        <label
-          htmlFor="problemtype"
-          className="text-sm block w-full mt-2 mb-1 text-left font-bold"
-        >
-          Problem Type
-        </label>
-        <select
-          name="problemtype"
-          className="block w-full mb-4 p-2 box-border rounded"
-          onChange={(e) =>
-            setValues({ ...values, problemtype: e.target.value })
-          }
-        >
-          <option value="mechanic">Mechanic</option>
-          <option value="electric">Electric</option>
-          <option value="utility">Utility</option>
-          <option value="production">Production</option>
-          <option value="metalworking">Metal Working</option>
-        </select>
-
-        <label
-          htmlFor="stopstatus"
-          className="text-sm block w-full mt-2 mb-1 text-left font-bold"
-        >
-          Stop Status
-        </label>
-        <select
-          name="stopstatus"
-          className="block w-full mb-4 p-2 box-border rounded"
-          onChange={(e) => setValues({ ...values, stopstatus: e.target.value })}
-        >
-          <option value="yes">Yes</option>
-          <option value="no">No</option>
-        </select>
-
-        <label
-          htmlFor="stopdate"
-          className="text-sm block w-full mt-2 mb-1 text-left font-bold"
-        >
-          Stop Date
-        </label>
-        <input
-          type="datetime-local"
-          className="block w-full p-2 box-border rounded-sm text-xs"
-          name="stopdate"
-          onChange={(e) => setValues({ ...values, stopdate: e.target.value })}
-        />
-
-        <label
-          htmlFor="startdate"
-          className="text-sm block w-full mt-2 mb-1 text-left font-bold"
-        >
-          Start Date
-        </label>
-        <input
-          type="datetime-local"
-          className="block w-full p-2 box-border rounded-sm text-xs"
-          name="startdate"
-          onChange={(e) => setValues({ ...values, startdate: e.target.value })}
-        />
-
-        <label
-          htmlFor="problemdescription"
-          className="text-sm block w-full mt-2 mb-1 text-left font-bold"
-        >
-          Problem Description
-        </label>
-        <textarea
-          name="problemdescription"
-          className={"block w-full mb-4 p-2 box-border rounded-sm text-xs"}
-          cols="50"
-          rows="10"
-          placeholder="Enter Problem Description"
-          onChange={(e) =>
-            setValues({ ...values, problemdescription: e.target.value })
-          }
-        ></textarea>
-        <div className="input-box">
-          <button
-            type={"submit"}
-            className={
-              "submit rounded-md m-4 p-3 text-white cursor-pointer  w-2/5 text-base bg-blue-800"
-            }
-          >
-            submit
-          </button>
-        </div>
-      </form>
+    <div className="d-flex justify-content-center alignt-items-center mt-3">
+      <div className="p-3 rounded w-50 border">
+        <h3 className="text-center">Submit Form</h3>
+        <form onSubmit={handleSubmit} className="row g-1">
+          <div className="col-12">
+            <label htmlFor="formcode" className="form-label">
+              Form Code / کد فرم
+            </label>
+            <input
+              type="text"
+              className="form-control rounded-0"
+              id="formcode"
+              placeholder="Enter Form Code"
+              onChange={(e) =>
+                setValues({ ...value, formcode: e.target.value })
+              }
+            />
+          </div>
+          <div>
+            <label htmlFor="section" className="form-label">
+              Section
+            </label>
+            <select
+              name="section"
+              id="section"
+              className="form-control rounded-0"
+              onChange={(e) => setValues({ ...value, section: e.target.value })}
+            >
+              <option value="chipper">Chipper</option>
+              <option value="conveyorline">Conveyor Line</option>
+              <option value="dryerairgraider">Dryer Air Grader</option>
+              <option value="refiner">Refiner</option>
+              <option value="beforepress">Before Press</option>
+              <option value="press">Press</option>
+              <option value="afterpress">After Press</option>
+              <option value="sandingrbs">Sanding</option>
+              <option value="coolingsystem">Cooling System</option>
+              <option value="steamboiler">Steam Boiler</option>
+              <option value="general">General</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="machinename" className="form-label">
+              Machine Name
+            </label>
+            <input
+              className="form-control rounded-0"
+              type="text"
+              name="machinename"
+              placeholder="Enter Machine Name"
+              id="machinename"
+              onChange={(e) =>
+                setValues({ ...value, machinename: e.target.value })
+              }
+            />
+          </div>
+          <div>
+            <label htmlFor="shift" className="form-label">
+              Shift
+            </label>
+            <select
+              name="shift"
+              id="shift"
+              className="form-control rounded-0"
+              onChange={(e) => setValues({ ...value, shift: e.target.value })}
+            >
+              <option value="a">A</option>
+              <option value="b">B</option>
+              <option value="c">C</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="operatorname" className="form-label">
+              Operator Name
+            </label>
+            <input
+              className="form-control rounded-0"
+              type="text"
+              name="operatorname"
+              id="operatorname"
+              placeholder="Enter Operator Name"
+              onChange={(e) =>
+                setValues({ ...value, operatorname: e.target.value })
+              }
+            />
+          </div>
+          <div>
+            <label htmlFor="formdate" className="form-label">
+              Form Date
+            </label>
+            <input
+              className="form-control rounded-0"
+              type="datetime-local"
+              name="formdate"
+              id="formdate"
+              onChange={(e) =>
+                setValues({ ...value, formdate: e.target.value })
+              }
+            />
+          </div>
+          <div>
+            <label htmlFor="problemtype" className="form-label">
+              Porblem Type
+            </label>
+            <select
+              className="form-control rounded-0"
+              name="problemtype"
+              id="problemtype"
+              onChange={(e) =>
+                setValues({ ...value, problemtype: e.target.value })
+              }
+            >
+              <option value="mechanic">Mechanic</option>
+              <option value="electric">Electric</option>
+              <option value="utility">Utility</option>
+              <option value="production">Production</option>
+              <option value="metalworking">Metal Working</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="stopstatus" className="form-label">
+              Stop Status
+            </label>
+            <select
+              className="form-control rounded-0"
+              name="stopstatus"
+              id="stopstatus"
+              onChange={(e) =>
+                setValues({ ...value, stopstatus: e.target.value })
+              }
+            >
+              <option value="yes">Yes</option>
+              <option value="no">No</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="stopdate" className="form-label">
+              Stop Date
+            </label>
+            <input
+              className="form-control rounded-0"
+              type="datetime-local"
+              name="stopdate"
+              id="stopdate"
+              onChange={(e) =>
+                setValues({ ...value, stopdate: e.target.value })
+              }
+            />
+          </div>
+          <div>
+            <label htmlFor="startdate" className="form-label">
+              Start Date
+            </label>
+            <input
+              className="form-control rounded-0"
+              type="datetime-local"
+              name="startdate"
+              id="startdate"
+              onChange={(e) =>
+                setValues({ ...value, startdate: e.target.value })
+              }
+            />
+          </div>
+          <div>
+            <label htmlFor="problemdescription" className="form-label">
+              Problem Description
+            </label>
+            <textarea
+              className="form-control rounded-0"
+              name="problemdescription"
+              id="problemdescription"
+              cols="30"
+              rows="10"
+              placeholder="Enter Problem Description"
+              onChange={(e) =>
+                setValues({ ...value, problemdescription: e.target.value })
+              }
+            ></textarea>
+          </div>
+          <div>
+            <button type={"submit"} className="btn btn-primary w-100">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default Submit;
