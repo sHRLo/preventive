14435233d877a6aa4b5ba4a572945ecb57bf5209
sHRLo <62970136+sHRLo@users.noreply.github.com>
2024-12-23 09:18:09 +0000

    Modify

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,6 +15,7 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
+
 import "./table.css";
 
 const Home = () => {
@@ -39,42 +40,7 @@ const Home = () => {
       pv: 2400,
       amt: 2400,
     },
-    {
-      name: "Page B",
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: "Page C",
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: "Page D",
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: "Page E",
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: "Page F",
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: "Page G",
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
+
   ];
 
   return (
@@ -85,7 +51,12 @@ const Home = () => {
             <h3>EM</h3>
             <CgDanger className="card_icon" />
           </div>
-          <h1>255</h1>
+          <h1>{
+            operator.map((c,index) => (
+             <h1 key={index}>
+              <h1>{c.formcode}</h1>
+             </h1>
+            ))}</h1>
         </div>
         <div className="card">
           <div className="card-inner">
